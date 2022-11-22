982e1856fb7e606e9f0b9eab34765c5eca1bfda7
Anton Sizikov <asizikov@github.com>
2022-11-22 16:00:18 +0000

    🏃‍♀️ add test script

---
+++ prepare-image.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo 'Going to create a postcard'
+
+POST_CARD_RECIEVER_NAME=$1
+POSTCARD_TEMPLATE=$2.jpg
+TEMPLATE_PATH=templates/$POSTCARD_TEMPLATE
+
+
+convert -pointsize 60 -fill red -font Z003-Medium-Italic -draw "text 500,920 'Happy New Year! Dear $POST_CARD_RECIEVER_NAME'" $TEMPLATE_PATH output/postcard_out.jpg
+
+
+
+echo 'your postcard is ready 📬'
